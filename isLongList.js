a485a7716ab80e0ce79a3a1202a1738af3a9f60e
Jill Kidman <jillkidman@Jills-MacBook-Air.local>
2022-02-18 12:06:49 -0500

    refactored isLongList

--- isLongList.js
+++ isLongList.js
@@ -1,11 +1,8 @@
 //4. `isLongList.js`: prompts the user for an array then logs out a boolean indicating whether the array's length is at least 10
-    const prompt = require("prompt-sync")({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
 let userArray = JSON.parse(prompt("Enter an array: "));
 
+let atLeast10 = (userArray.length >= 10);
 
-if (userArray.length < 10){
-    console.log("The length of this array is less than 10.");}
-    else if (userArray.length >= 10){
-        console.log("The length of this array is at least 10 or more.")
-    }
+console.log(atLeast10);
